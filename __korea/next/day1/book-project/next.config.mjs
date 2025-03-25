52f25548186d5140dbad764a7069f94ab7dd5680
hyobin15 <7975099@hanmail.net>
2025-03-25 10:02:13 +0900

    Updates

--- __korea/next/day1/book-project/next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false, // 버그 발생으로 false로 변경
-};
-
-export default nextConfig;
--- __korea/next/day1/book-project/src/pages/search/setting.jsx
+++
@@ -1,5 +0,0 @@
-export default function Page(){
-  return(
-    <h1>Search/Setting</h1>
-  )
-}
