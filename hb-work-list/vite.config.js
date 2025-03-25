6f32fcad4c938d09c8990b5cdfec0699b8b37c5b
hyobin15 <7975099@hanmail.net>
2025-03-25 13:04:40 +0900

    gitignore-> build 폴더 제외, vite.config에 build 폴더 경로 추가

--- hb-work-list/vite.config.js
+++ hb-work-list/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/study/hb-work-list/', // GitHub Pages에서 배포되는 서브 디렉토리
+  build: {
+    outDir: 'build', // 빌드 결과물을 'build' 폴더에 저장
+  },
 })
