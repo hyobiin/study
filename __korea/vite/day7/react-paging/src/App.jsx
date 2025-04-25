4b60dc74c106b3694fdaab9254a537280105c426
hyobin <7975099@hanmail.net>
2025-04-25 20:57:53 +0900

    동적 폼 추가 useFormik, FieldArray, Form, FormikProvider 사용

--- __korea/vite/day7/react-paging/src/App.jsx
+++ __korea/vite/day7/react-paging/src/App.jsx
@@ -1,8 +1,7 @@
-import { FieldArray, useFormik } from "formik"
+import { FieldArray, FormikProvider, useFormik, Form } from "formik"
 import Check from "./components/form/Check"
 import CustomChkSelect from "./components/form/CustomChkSelect"
 import Radio from "./components/form/Radio"
-import Form from "./pages/form"
 import CustomDate from "./components/form/CustomDate"
 // import PagingPosts from "./PagingPosts_btn"
 
@@ -18,7 +17,11 @@ function App() {
           radio: "false",
         }
       ]
-    }
+    },
+    onSubmit: (values) => {
+      console.log('제출 값', values)
+    },
+
   })
 
   return (
@@ -26,35 +29,39 @@ function App() {
       {/* <PagingPosts /> */}
 
       {/* 동적 추가 테스트 */}
-      <FieldArray name="addForms">
-        {({ push, remove}) => (
-          <>
-            {formData.values.addForms.map((item, index) => (
-              <div key={index}>
-                <Check name={`addForms[${index}].check`} type="checkbox" label="체크해주세요" checked={item.check} onChange={formData.handleChange} />
-                <Check name={`addForms[${index}].check2`} type="checkbox" label="체크해주세요12" checked={item.check2} onChange={formData.handleChange} />
-                <Radio name={`addForms[${index}].radio`} type="radio" label="라디오 버튼" value="true" checked={item.radio === 'true'} onChange={formData.handleChange} />
-                <Radio name={`addForms[${index}].radio`} type="radio" label="라디오 버튼2" value="false" checked={item.radio === 'false'} onChange={formData.handleChange} />
-              </div>
-            ))}
-            <button
-              type="button"
-              onClick={() =>
-                push({ check: false, check2: false, radio: "false"})
-              }
-            >클릭 시 폼 추가
-            </button>
-          </>
-        )}
-      </FieldArray>
+      <FormikProvider value={formData}>
+        <Form onSubmit={formData.handleSubmit}>
+          <FieldArray name="addForms">
+            {({ push, remove}) => (
+              <>
+                {formData.values.addForms.map((item, index) => (
+                  <div key={index}>
+                    <Check name={`addForms[${index}].check`} type="checkbox" label="체크해주세요" checked={item.check} onChange={formData.handleChange} />
+                    <Check name={`addForms[${index}].check2`} type="checkbox" label="체크해주세요12" checked={item.check2} onChange={formData.handleChange} />
+                    <Radio name={`addForms[${index}].radio`} type="radio" label="라디오 버튼" value="true" checked={item.radio === 'true'} onChange={formData.handleChange} />
+                    <Radio name={`addForms[${index}].radio`} type="radio" label="라디오 버튼2" value="false" checked={item.radio === 'false'} onChange={formData.handleChange} />
+                  </div>
+                ))}
+                <button
+                  type="button"
+                  onClick={() =>
+                    push({ check: false, check2: false, radio: "false"})
+                  }
+                >클릭 시 폼 추가
+                </button>
+              </>
+            )}
+          </FieldArray>
+        </Form>
+      </FormikProvider>
       {/* 동적 추가 테스트 */}
 
       <pre>{JSON.stringify(formData.values, null, 2)}</pre>
 
-      <CustomChkSelect />
-      <Form />
+      {/* <CustomChkSelect /> */}
+      {/* <Form /> */}
 
-      <CustomDate />
+      {/* <CustomDate /> */}
     </>
   )
 }
